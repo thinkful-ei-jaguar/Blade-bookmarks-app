e9f50c5d0bcc2cd7b88f19dfcf7749ba4394dc17
Blade Boles <bladerboles@gmail.com>
2019-12-13 11:39:56 -0700

    Added scripts and webpack config.

--- formErrorStoreObject.js
+++ formErrorStoreObject.js
@@ -4,3 +4,5 @@ const store = {
   error: 'error.message', //It will contain the message error generated.  This will similar to how it is in the api shopping list.
   filter: 0 //Unless the error happened while filter was applied
 };
+
+
